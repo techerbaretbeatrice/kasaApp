a1c7f55d32531f0b6dc03b2dfa417da939bc8966
bbeatrice <techerbaretbeatrice@gmail.com>
2022-10-10 09:11:06 +0200

    initialisation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="about">About</Link>
+              </li>
+              <li>
+                <Link to="produit">Produit</Link>
+              </li>
+              <li>
+                <Link to="*">Page Not Found</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="about/*" element={<About />}>
+            </Route>
+            <Route path="*" element={< PageNotFound />}>
+            </Route>
+            <Route path="/" element={<Home />}>
+            </Route>
+            <Route path="produit/*" element={<Produit />}>
+            </Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
+
+function Home() {
+  return <nav>
+    <Link to="/">Home</Link>
+  </nav>;
+}
+
+function About() {
+  return <nav> <Link to="about">About</Link></nav>;
+}
+
+function PageNotFound() {
+  return <nav><Link to="*">Page not Found</Link></nav>;
+}
+function Produit() {
+  return <nav><Link to="produit/*">Produit</Link></nav>;
+}
+
 
 export default App;
